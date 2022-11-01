745f38116cb011fa3ac2521daf0423053978fc80
Jonathan Schneider <jonathan@jott.media>
2022-11-01 22:36:30 +0100

    add sidebar

--- content/.vitepress/config.js
+++ content/.vitepress/config.js
@@ -1,5 +1,38 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
-  outDir: '../docs'
+  outDir: '../docs',
+  themeConfig: {
+    sidebar: [
+      {
+        text: 'Community',
+        items: [
+          { text: 'Mitglieder', link: '/community/mitglieder' },
+          { text: 'Team', link: '/community/team' },
+          { text: 'Partner', link: '/community/partner' },
+        ]
+      },
+      {
+        text: 'Postcast',
+        items: [
+          { text: 'Aktuell', link: '/podcast/aktuell' },
+          { text: 'Archiv', link: '/podcast/archiv' },
+        ]
+      },
+      {
+        text: 'Anfrage',
+        items: [
+          { text: 'Kontakt', link: '/anfrage/kontakt' },
+          { text: 'Kooperation', link: '/anfrage/kooperation' }
+        ]
+      },
+      {
+        text: 'Legal',
+        items: [
+          { text: 'Impressum', link: '/impressum' },
+          { text: 'Datenschutz', link: '/datenschutz' },
+        ]
+      },
+    ]
+  }
 })
